524544bae414ab1f5d5bf940f09ba765e01c8395
Gourav Mallick <your-email@example.com>
2025-12-16 23:15:24 +0530

    for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/pdfCraft/",
+    base: "/pdfCraft",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
